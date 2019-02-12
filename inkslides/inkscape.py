24d405e263ac791385e8cf882b06e46130e9b9c4
Johannes Graeter <johannes.graeter@googlemail.com>
2019-02-12 14:51:22 +0100

    Use subprocess.call to remove dep of inkscape printing >

--- inkslides/inkscape.py
+++ inkslides/inkscape.py
@@ -7,33 +7,14 @@ class InkscapeWorker(multiprocessing.Process):
         super(InkscapeWorker, self).__init__()
         self.queue = queue
 
-    def wait_for_inkscape(self):
-        while self.ink.stdout.read(1) != b'>':
-            pass
-
     def run(self):
-        # this is our inkscape worker
-        self.ink = subprocess.Popen(['inkscape', '--shell'],
-                                    stdin=subprocess.PIPE,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.STDOUT)
-
-        # first, wait for inkscape startup
-        self.wait_for_inkscape()
-
         for svg_file, pdf_file_name, cached in iter(self.queue.get, None):
-
             # main working loop of the inkscape process
-            # we need to wait for ">" to see whether inkscape is ready.
-            # The variable ready keeps track of that.
-
+            # subprocess.call waits until process is finished
             if not cached:
-                command = '-A "{1}" "{0}"\n'.format(svg_file, pdf_file_name)
-                self.ink.stdin.write(command.encode("UTF-8"))
-                self.ink.stdin.flush()
-
-                self.wait_for_inkscape()
-
+                command = 'inkscape -A {1} -f {0}'.format(svg_file, pdf_file_name)
+                ret = subprocess.call(command.split(" "))
+                assert (ret == 0)
                 print("  Converted {0}".format(pdf_file_name))
             else:
                 print("  Skipping {0}".format(pdf_file_name))
