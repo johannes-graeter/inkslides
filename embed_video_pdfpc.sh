bd6856434c403a38eb25cd909d7bdfb167027ca5
Johannes Graeter <johannes.graeter@googlemail.com>
2019-02-14 18:04:55 +0100

    Add embed for pdfpc, which works with autoplay but only full screen. Normal one should wrk with okular and pdfpc but no autoplay and start.

--- embed_video_pdfpc.sh
+++ embed_video_pdfpc.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+start_s ${4:"0"}
 stripped_name=${3%%.pdf}_tmp
 
 echo "writing to $stripped_name.pdf"
 
 echo "\documentclass[]{beamer}
-\usepackage{multimedia}
+%\usepackage{multimedia}
 \usepackage{graphicx}
 \usepackage[absolute,overlay]{textpos}
 
-%\geometry{margin=0cm, hmargin=0cm}
 \geometry{paperwidth=238.125mm,paperheight=178.594mm, margin=0cm, hmargin=0cm}
 \setbeamersize{text margin left=0cm,text margin right=0cm} 
 
-\usebackgroundtemplate{\includegraphics[width=\paperwidth]{$1}}
+%\usebackgroundtemplate{\includegraphics[width=\paperwidth]{$1}}
 \begin{document}
 {
   \setbeamertemplate{footline}{}
   \begin{frame}[plain]
-    \begin{textblock*}{0.99\paperwidth}(0.01\paperwidth,0.2\paperheight)% {block width} (coords)
+    \begin{textblock*}{\paperwidth}(0.0cm,0.0cm)% {block width} (coords)
       \centering
-      \movie[autostart, start=30s, duration=10s, showcontrols=true, width=0.7\paperwidth, height=0.7\paperheight]{}{$2}
+      \href{run:$2?autostart&loop&start=$4}{\includegraphics[width=0.98\paperwidth]{$1}}
+      %\movie[autostart, start=30s, duration=10s, showcontrols=true, width=0.7\paperwidth, height=0.7\paperheight]{}{$2}
     \end{textblock*}
   \end{frame}
 }
